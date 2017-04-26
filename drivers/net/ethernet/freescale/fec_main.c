caca4f9dcb6b53c1b47fce0f6572104e39c0956c
Stefan Wahren <stefan.wahren@i2se.com>
2017-04-26 17:02:49 +0200

    net: fec: Reset ethernet PHY whenever the enet_out clock is being enabled
    If a PHY uses ENET_OUT as reference clock, it may need a RESET to get
    functional after the clock had been disabled.
    
    Failure to do this results in the link state constantly toggling
    between up and down:
    fec 02188000.ethernet eth0: Link is Up - 100Mbps/Full - flow control rx/tx
    fec 02188000.ethernet eth0: Link is Down
    fec 02188000.ethernet eth0: Link is Up - 100Mbps/Full - flow control rx/tx
    fec 02188000.ethernet eth0: Link is Down
    [...]
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -68,6 +68,7 @@
 
 static void set_multicast_list(struct net_device *ndev);
 static void fec_enet_itr_coal_init(struct net_device *ndev);
+static int fec_reset_phy(struct platform_device *pdev);
 
 #define DRIVER_NAME	"fec"
 
@@ -1847,6 +1848,8 @@ static int fec_enet_clk_enable(struct net_device *ndev, bool enable)
 		if (ret)
 			goto failed_clk_enet_out;
 
+		fec_reset_phy(fep->pdev);
+
 		if (fep->clk_ptp) {
 			mutex_lock(&fep->ptp_clk_mutex);
 			ret = clk_prepare_enable(fep->clk_ptp);
@@ -3190,10 +3193,11 @@ static int fec_enet_init(struct net_device *ndev)
 #ifdef CONFIG_OF
 static int fec_reset_phy(struct platform_device *pdev)
 {
-	int err, phy_reset;
-	bool active_high = false;
+	int err;
 	int msec = 1;
 	struct device_node *np = pdev->dev.of_node;
+	struct net_device *ndev = platform_get_drvdata(pdev);
+	struct fec_enet_private *fep = netdev_priv(ndev);
 
 	if (!np)
 		return 0;
@@ -3203,28 +3207,14 @@ static int fec_reset_phy(struct platform_device *pdev)
 	if (!err && msec > 1000)
 		msec = 1;
 
-	phy_reset = of_get_named_gpio(np, "phy-reset-gpios", 0);
-	if (phy_reset == -EPROBE_DEFER)
-		return phy_reset;
-	else if (!gpio_is_valid(phy_reset))
-		return 0;
-
-	active_high = of_property_read_bool(np, "phy-reset-active-high");
-
-	err = devm_gpio_request_one(&pdev->dev, phy_reset,
-			active_high ? GPIOF_OUT_INIT_HIGH : GPIOF_OUT_INIT_LOW,
-			"phy-reset");
-	if (err) {
-		dev_err(&pdev->dev, "failed to get phy-reset-gpios: %d\n", err);
-		return err;
-	}
+	gpio_set_value_cansleep(fep->phy_reset, fep->active_high);
 
 	if (msec > 20)
 		msleep(msec);
 	else
 		usleep_range(msec * 1000, msec * 1000 + 1000);
 
-	gpio_set_value_cansleep(phy_reset, !active_high);
+	gpio_set_value_cansleep(fep->phy_reset, fep->active_high);
 
 	return 0;
 }
@@ -3418,6 +3408,23 @@ fec_probe(struct platform_device *pdev)
 	pm_runtime_get_noresume(&pdev->dev);
 	pm_runtime_set_active(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
+
+	if (np) {
+		ret = of_get_named_gpio(np, "phy-reset-gpios", 0);
+		if (ret == -EPROBE_DEFER)
+			goto failed_reset;
+
+		if (gpio_is_valid(ret)) {
+			fep->phy_reset = ret;
+			fep->active_high = of_property_read_bool(np, "phy-reset-active-high");
+
+			ret = devm_gpio_request_one(&pdev->dev, fep->phy_reset,
+					fep->active_high ? GPIOF_OUT_INIT_HIGH : GPIOF_OUT_INIT_LOW,
+					"phy-reset");
+			if (ret)
+				dev_err(&pdev->dev, "failed to get phy-reset-gpios: %d\n", ret);
+		}
+	}
 
 	ret = fec_reset_phy(pdev);
 	if (ret)
